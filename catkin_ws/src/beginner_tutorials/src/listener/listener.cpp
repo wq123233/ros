11cb2c1d85f207aaaf1b0d8c8e430f5b9ab3915e
wq123233 <yxwq6318@163.com>
2020-03-02 15:19:21 +0800

    new program

--- catkin_ws/src/beginner_tutorials/src/listener/listener.cpp
+++ catkin_ws/src/beginner_tutorials/src/listener/listener.cpp
@@ -1,7 +1,7 @@
 #include"ros/ros.h"
 #include"std_msgs/String.h"
 
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
+void chatterCallback(const std_msgs::String::ConstPtr& msg) // ::和/ 表示的是在文件夹下 ->表示文件夹下的某个变量 rosmsg 能显示文件夹下的变量
 {
     ROS_INFO("l:[%s]", msg->data.c_str());
 }
@@ -15,3 +15,4 @@ int main(int argc, char **argv)
     ros::spin(); //  等待zhixing回调函数 
     return 0;
 }
+
---
+++ catkin_ws/src/beginner_tutorials/src/talker/talker_para.cpp
@@ -0,0 +1,39 @@
+
+#include "ros/ros.h"    //ros.h 包含ros程序常用的头文件。
+#include "beginner_tutorials/Num.h"  //引用服务类型头文件
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "talker_para");  //初始化ros，并命令节点名。
+  ros::NodeHandle n("~");    //初始化节点，调用ros api接口句柄。
+  //定义一个客户端
+  ros::Publisher client = n.advertise<beginner_tutorials::Num>("chatter_para",1);
+  ros::Rate loop_rate(1);  //设置发布频率
+  int count = 0;    //发布请求计数
+  beginner_tutorials::Num num;   //定义服务变量
+  int32_t a,b;
+  while (ros::ok()){    //检测节点是否正常运行
+    //srv.request.a = count;      //更新服务变量值。
+    //srv.request.b = count+1;
+    if(n.getParam("avalue",a))
+    {
+      num.a = a;
+    }else
+    {
+      ROS_WARN("fail get avalue");
+    }
+    if(n.getParam("bvalue",b))
+    {
+      num.b = b;
+    }else
+    {
+      ROS_WARN("fail get bvalue");
+    }
+    ROS_INFO("%d %d",num.a,num.b);
+    client.publish(num);
+    ros::spinOnce();    //处理回调函数，会返回，这里也可以不加，因为此节点没有回调函数。
+    loop_rate.sleep();  //与Line 10配套使用，用于控制发布频率。
+    ++count;            //消息计数。
+  }
+  return 0;
+}
