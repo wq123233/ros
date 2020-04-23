7ab532cdd924befe9fb738536e6777a5df96b10a
wq123233 <yxwq6318@163.com>
2020-04-23 22:54:01 +0800

    learing

--- catkin_ws/src/beginner_tutorials/src/listener/listener.cpp
+++ catkin_ws/src/beginner_tutorials/src/listener/listener.cpp
@@ -1,17 +1,35 @@
-#include"ros/ros.h"
-#include"std_msgs/String.h"
+// #include"ros/ros.h"
+// #include"std_msgs/String.h"
 
-void chatterCallback(const std_msgs::String::ConstPtr& msg) // ::和/ 表示的是在文件夹下 ->表示文件夹下的某个变量 rosmsg 能显示文件夹下的变量
+// void chatterCallback(const std_msgs::String::ConstPtr& msg) // ::和/ 表示的是在文件夹下 ->表示文件夹下的某个变量 rosmsg 能显示文件夹下的变量
+// {
+//     ROS_INFO("l:[%s]", msg->data.c_str());
+// }
+
+// int main(int argc, char **argv)
+// {
+//     ros::init(argc,argv,"listener");
+//     ros::NodeHandle n;
+//     ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+//     ros::spin(); //  等待zhixing回调函数 
+//     return 0;
+// }
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+void  chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
     ROS_INFO("l:[%s]", msg->data.c_str());
 }
+
 
 int main(int argc, char **argv)
 {
     ros::init(argc,argv,"listener");
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-    ros::spin(); //  等待zhixing回调函数 
+    ros::spin();  // 一般的 监听者都这么写 直接进入回调函数 不在执行 return 0
     return 0;
-}
 
+}
--- catkin_ws/src/beginner_tutorials/src/talker/talker.cpp
+++ catkin_ws/src/beginner_tutorials/src/talker/talker.cpp
@@ -1,10 +1,40 @@
+// #include "ros/ros.h"
+// #include "std_msgs/String.h"
+// #include <sstream>
+
+// int main(int argc, char **argv)
+// {
+//     ros::init(argc,argv,"talker");
+//     ros::NodeHandle n;
+//     ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter",1);
+
+//     ros::Rate loop_rate(10);
+//     int count = 0;
+//     while (ros::ok())
+//     {
+//         /* code */
+//         std_msgs::String msg;
+
+//         std::stringstream ss;
+//         ss << "hello world!!" << count;
+//         msg.data = ss.str();
+//         ROS_INFO("%s",msg.data.c_str());
+//         chatter_pub.publish(msg);
+//         ros::spinOnce();
+//         loop_rate.sleep();
+//         ++count;
+
+//     }
+//     return 0;
+// }
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <sstream>
 
 int main(int argc, char **argv)
 {
-    ros::init(argc,argv,"talker");
+    ros::init(argc, argv, "talker");
     ros::NodeHandle n;
     ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter",1);
 
@@ -16,14 +46,13 @@ int main(int argc, char **argv)
         std_msgs::String msg;
 
         std::stringstream ss;
-        ss << "hello world!!" << count;
+        ss << "nihao shijie !" << count;
         msg.data = ss.str();
-        ROS_INFO("%s",msg.data.c_str());
+        ROS_INFO("%s", msg.data.c_str());
         chatter_pub.publish(msg);
-        ros::spinOnce();
+        ros::spinOnce();  // 这里可以不加，处理完了回调函数以后会再处理主函数
         loop_rate.sleep();
         ++count;
-
     }
     return 0;
 }
